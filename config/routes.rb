9c030faa192a1b99eb96ed2d25d93414b9abb6a3
Ann Clawson <annmasonclawson@gmail.com>
2023-10-24 10:26:28 -0700

    Adds routes for 'all products' and each product

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/all_products", controller: "products", actions: "all_products"
+  get "/Pacer", controller: "products", actions: "Pacer"
+  get "/Hot_Dog", controller: "products", actions: "Hot_Dog"
+  get "/Pathos", controller: "products", actions: "Pathos"
+  get "/Soda_Pop", controller: "products", actions: "Soda_Pop"
+  get "/Bubble_Gum", controller: "products", actions: "Bubble_Gum"
 end
