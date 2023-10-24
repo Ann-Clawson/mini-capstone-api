a65c285070b81b1cfe600ac9ecd3fdf0dd0abcad
Ann Clawson <annmasonclawson@gmail.com>
2023-10-24 09:56:32 -0700

    Rails db:seed adds items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
+car = Product.new(name: "Pacer", price: 2345, img_url: "https://www.digitaltrends.com/wp-content/uploads/2016/10/waynes-world-amc-pacer.jpg?fit=720%2C720&p=1", description: "Reliable, seats four singing 'Bohemian Rhapsody'")
+car.save
+
+hot_dog = Product.new(name: "Hot Dog", price: 1.50, img_url: "https://media.cnn.com/api/v1/images/stellar/prod/181003190618-costco-hot-dog.jpg?q=w_4032,h_2268,x_0,y_0,c_fill", description: "Fits the bun")
+hot_dog.save
+
+houseplant = Product.new(name: "Pathos", price: 15.00, img_url: "https://images.squarespace-cdn.com/content/v1/54fbb611e4b0d7c1e151d22a/1610072931440-5JZIEFJ96XSX748F10H3/Pothos-Houseplant.jpg?format=1500w", description: "Well mannered, easy care for")
+houseplant.save
+
+soda_pop = Product.new(name: "Soda Pop", price: 2.50, img_url: "https://images.vat19.com/pickle-soda/pickle-soda-pop-drinking.jpg", description: "Fizzy")
+soda_pop.save
+
+bubble_gum = Product.new(name: "Bubble Gum", price: 0.50, img_url: "https://townsquare.media/site/65/files/2014/03/RS3127_177272954-scr.jpg?w=980&q=75", description: "Chewy")
+bubble_gum.save
+
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
