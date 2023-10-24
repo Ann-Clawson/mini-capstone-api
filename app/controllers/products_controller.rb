947c457df3e8641e87580f06db0ca6a82cb8cd4c
Ann Clawson <annmasonclawson@gmail.com>
2023-10-24 10:34:15 -0700

    Adds controllers for all and each product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,31 @@
 class ProductsController < ApplicationController
+  def all_products
+    @products = Product.all
+    render :index
+  end
+
+  def Pacer
+    @product = Product.name("Pacer")
+    render :show
+  end
+
+  def Hot_Dog
+    @product = Product.name("Hot_Dog")
+    render :show
+  end
+
+  def Pathos
+    @product = Product.name("Pathos")
+    render :show
+  end
+
+  def Soda_Pop
+    @product = Product.name("Soda_Pop")
+    render :show
+  end
+
+  def Bubble_Gum
+    @product = Product.name("Bubble_Gum")
+    render :show
+  end
 end
