670cb763862d47316b5516774b7e241b3d9417ad
Ann Clawson <annmasonclawson@gmail.com>
2023-10-25 10:57:52 -0700

    Adds create method and connect route and controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,28 +9,14 @@ class ProductsController < ApplicationController
     render :show
   end
 
-  # def pacer
-  #   @product = Product.first
-  #   render :show
-  # end
-
-  # def hot_dog
-  #   @product = Product.second
-  #   render :show
-  # end
-
-  # def pathos
-  #   @product = Product.third
-  #   render :show
-  # end
-
-  # def soda_pop
-  #   @product = Product.fourth
-  #   render :show
-  # end
-
-  # def bubble_gum
-  #   @product = Product.fifth
-  #   render :show
-  # end
+  def create
+    @product = Product.new(
+      name: "Coffee",
+      price: 2,
+      img_url: "https://www.eatingwell.com/thmb/wBKeO_wzc97ngtNFph1c2Y0wHfE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/What-Happens-To-Your-Body-When-You-Drink-Coffee-Every-Day-2000-ead0e982c03b4a50a4e115f145069b8b.jpg",
+      description: "full throttle",
+    )
+    @product.save
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/products.json", controller: "products", action: "all_products"
   get "/products/:id" => "products#products"
+  post "products" => "products#create"
   # get "/pacer", controller: "products", action: "pacer"
   # get "/hot_dog", controller: "products", action: "hot_dog"
   # get "/pathos", controller: "products", action: "pathos"
