b3eb53f5052d15df96b50112ea04d7634445e751
Ann Clawson <annmasonclawson@gmail.com>
2023-10-26 14:00:47 -0700

    changes price of bubble gum, rounds tax to floor

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,14 @@ class Product < ApplicationRecord
   end
 
   def tax
-    price * 0.09
+    (price * 0.09).floor
   end
 
   def total
-    price + (price * 0.09)
+    price + tax
+  end
+
+  def friendly_created_at
+    created_at.strftime("%A, %d %b %Y %l:%M %p")
   end
 end
--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -6,5 +6,6 @@ json.description product.description
 json.is_discounted? product.is_discounted?
 json.tax product.tax
 json.total product.total
+json.friendly_created_at product.friendly_created_at
 json.created_at product.created_at
 json.updated_at product.updated_at
