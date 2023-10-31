9ab1e865bdd03a69f005c478260263166f4c3e49
Ann Clawson <annmasonclawson@gmail.com>
2023-10-31 11:11:36 -0700

    adds image route and controller to delete images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,7 @@
 class ImagesController < ApplicationController
+  def destroy
+    @image = Image.find_by(id: params["id"])
+    @image.destroy
+    render json: { message: "image successfully deleted" }
+  end
 end
