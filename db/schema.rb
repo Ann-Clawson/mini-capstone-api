148a1658fb1ea3aa1c182110b3c97112119f9ab9
Ann Clawson <annmasonclawson@gmail.com>
2023-10-31 10:48:57 -0700

    migrates new image model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_31_174449) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_31_174758) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_31_174449) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price", precision: 9, scale: 2
-    t.string "img_url"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
