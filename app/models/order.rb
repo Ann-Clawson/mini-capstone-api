3d29bcb8e8aa0654081893d60d54961b40d77d43
Ann Clawson <annmasonclawson@gmail.com>
2023-11-29 10:34:20 -0800

    creates associations with cartedproducts, orders, users, and products

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
-  belongs_to :product
+  has_many :carted_products
   # many people can order the product, in this BUY NOW situation where you can only buy one item at a time and not put them in a cart
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -37,6 +37,9 @@ class Product < ApplicationRecord
   has_many :category_products
   # has_many :categories, through: :category_products
 
+  has_many :carted_products
+  has_many :users, through: carted_products
+
   def categories
     category_products.map do |category_product|
       category_product.category.name
